b1bc7bbc05edbb23cf34b5f0748942d4db9cb47b
LEE-NS <ogawa9341@gmail.com>
2024-04-16 11:32:20 +0900

    방명록 댓글 기능 구현

--- script/script_visitors.js
+++ script/script_visitors.js
@@ -1,3 +1,4 @@
+/* <조원의 이름에 따라 '/' 이후에 오는 이름이 바뀌는 기능> */
 let urlParams = new URLSearchParams(window.location.search); // URL에서 물음표(?) 이후에 오는 부분 가져오기
 let referrer = urlParams.get('referrer'); // referrer 속성의 값 가져오기
 let username = null; // 페이지에 따라 바뀔 이름
@@ -17,25 +18,50 @@ switch(referrer) {
         username = "한효림";
 }
 
-/* 
-<기능>
-방명록 : 
-    1. input에서 사용자가 입력한 텍스트를 받아온다.
-    2. '등록' 버튼을 누르면 input에서 작성된 텍스트가 하단의 ul태그 안으로 들어온다.
-    3. 댓글이 하단에 등록될 때, 해당 댓글을 작성한 연도, 날짜, 시간이 자동으로 함께 기록된다.
-    3. '수정' 버튼을 누르면 해당 위치에서 텍스트 수정이 가능해지며, 새롭게 생성된 '등록' 버튼을 누를 시 변경된 내용으로 갱신된다.
-    4. '삭제' 버튼을 누르면 해당 댓글이 삭제된다.
 
-*/
-
+/* <댓글을 다는 기능> */
 let writeInput = document.querySelector('.write input');
 let writeBtn = document.querySelector('.write button');
 let replySectionList = document.querySelector('.reply_section ul');
 
-/* function printInput() {
-    let textOfInput = writeInput.value;
-    replySectionList.innerText += 
-} */
+function reply() {
+    let today = new Date();
+    let year = today.getFullYear();
+    let month = ('0' + (today.getMonth() + 1)).slice(-2);
+    let day = ('0' + today.getDate()).slice(-2);
+    let hours = ('0' + today.getHours()).slice(-2);
+    let minutes = ('0' + today.getMinutes()).slice(-2);
+    let seconds = ('0' + today.getSeconds()).slice(-2);
 
-writeBtn.addEventListener('click', () => {printInput()})
-//'등록하기' 버튼 클릭 시 input의 텍스트 내용 들어오는 것 확인됨
+    let dateString = year + '.' + month + '.' + day;
+    let timeString = hours + ':' + minutes + ':' + seconds;
+
+    let html = `
+    <li>
+        <div class="texts">
+            <p class="texts_reply">${writeInput.value}</p>
+            <p class="texts_inf">
+                <span>${dateString}</span>
+                <span>${timeString}</span>
+                <button class="fix">수정</button>
+                <button class="delete">삭제</button>
+            </p>
+        </div>
+    </li>
+    `;
+
+    replySectionList.innerHTML += html;
+}
+//날짜와 시간이 사용자가 입력한 텍스트와 함께 등록되도록 한다.
+
+
+writeBtn.addEventListener('click', () => {
+    if (writeInput.value === '') {
+        alert('내용을 입력해주세요.')
+        writeInput.focus();
+        return
+    }
+    reply()
+    writeInput.value = '';
+})
+// 클릭하면 input.value의 내용이 댓글란에 등록된다. 이후 input.value를 비운다. 공백인 경우 alert창이 뜬 후 input에 focus가 생긴다.
