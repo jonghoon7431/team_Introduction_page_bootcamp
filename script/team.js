682ec667f99de4d5b90e29a0b5b7d7a488a8859a
hj7321 <oboki2001@naver.com>
2024-04-18 13:52:31 +0900

    팀 소개 페이지 UX 보완

--- script/team.js
+++ script/team.js
@@ -1,15 +1,18 @@
-const aNode = document.querySelector(".toggle");
-const pNodes = document.querySelectorAll("p");
-console.log(pNodes);
+const aNodes = document.querySelectorAll(".toggle");
+const divNodes = [...document.querySelectorAll(".container")];
 
-// a 태그를 클릭할 때마다 p 태그가 열고 닫히도록 설정함
-aNode.addEventListener("click", () => {
-  pNodes.forEach((pNode) => {
-    console.log(pNode.style.display);
-    let display = pNode.style.display;
-    if(display === "none")
-      display = "block";
-    else
-      display = "none";
+// a 태그를 클릭할 때마다 div 태그가 열고 닫히도록 설정함
+aNodes.forEach((a) => {
+  a.addEventListener("click", (event) => {
+    let url = event.target.href;
+    let fragUrl = url.split("#");
+    let word = fragUrl[fragUrl.length - 1];
+    divNodes.filter((div) => {
+      if (div.id === word && div.className.includes("hidden")) {
+        div.classList.remove("hidden");
+      } else if (div.id === word && !div.className.includes("hidden")) {
+        div.classList.add("hidden");
+      }
+    });
   });
 });
